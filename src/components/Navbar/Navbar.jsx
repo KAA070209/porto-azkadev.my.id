83c10da7043679abcd5cce123b4613321f438255
m.azkanabhan <m.azkanabhan07@gmail.com>
2026-09-05 21:10:20 +0700

    new

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -151,6 +151,18 @@ export default function Navbar() {
               {link.label}
             </Link>
           ))}
+          <a
+            href="/cv.pdf"
+            className="navbar-cv-btn navbar-cv-btn-mobile"
+            onClick={handleCvClick}
+          >
+            Download CV
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+              <polyline points="7 10 12 15 17 10"></polyline>
+              <line x1="12" y1="15" x2="12" y2="3"></line>
+            </svg>
+          </a>
         </div>
 
         <a
